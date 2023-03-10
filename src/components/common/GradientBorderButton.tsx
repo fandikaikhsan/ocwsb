0edaec09d94856bcbb17b0ff4c423743c1d01227
Fandika <fandika.ikhsan@gmail.com>
2023-03-10 11:27:29 +0700

    feat: add hover behavior in solid button to bg orange

--- src/components/common/GradientBorderButton.tsx
+++ src/components/common/GradientBorderButton.tsx
@@ -9,7 +9,7 @@ const GradientBorderButton: FC<GradientBorderButtonProps> = (props) => {
   const { onClick = () => {}, text = "" } = props
   return (
     <button
-      className="bg-transparent border-2 border-y-black rounded-lg text-white text-sm py-2 px-16"
+      className="bg-transparent hover:bg-orange-500 hover:border-orange-500 border-2 border-y-black rounded-lg text-white text-sm py-2 px-16"
       onClick={() => onClick()}
     >
       {text}
