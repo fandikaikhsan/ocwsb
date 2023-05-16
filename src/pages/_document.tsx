06550d26204d64b2b4bf97efc15ccfdffd25d101
Fandika <fandika.ikhsan@gmail.com>
2023-05-17 01:08:36 +0700

    feat: add meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <title>OCWSB</title>
+        {/* <meta name="description" content="Your site description" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
