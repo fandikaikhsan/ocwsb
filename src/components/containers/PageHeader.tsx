5bd9b525079667b89bfc1b8b9da12db36ecf29b7
Fandika <fandika.ikhsan@gmail.com>
2023-06-19 12:51:51 +0700

    feat: add header type title or detail

--- src/components/containers/PageHeader.tsx
+++ src/components/containers/PageHeader.tsx
@@ -3,16 +3,23 @@ import React, { FC } from "react"
 interface PageHeaderProps {
   title: string
   description: string
+  type: "title" | "detail"
 }
 
-const PageHeader: FC<PageHeaderProps> = ({ title, description }) => {
+const PageHeader: FC<PageHeaderProps> = ({ title, description, type }) => {
+  type = type || "title"
+
   return (
     <>
       <div className="flex flex-col gap-4 justify-center items-center w-full bg-[#74290E] h-56">
         <div className="text-[2rem] text-white font-maqin">{title}</div>
-        <div className="text-sm text-white text-center w-[75%] md:w-[50%]">
-          {description}
-        </div>
+        {type === "detail" ? (
+          <div className="text-sm text-white text-center w-[75%] md:w-[50%]">
+            {description}
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   )
