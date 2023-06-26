8da166a1a2e3620ed6e8ca125a342029fd5431eb
Fandika <fandika.ikhsan@gmail.com>
2023-06-26 14:41:13 +0700

    feat: add fetching function to fetch service data

--- src/app/services/[id]/page.tsx
+++ src/app/services/[id]/page.tsx
@@ -1,35 +1,27 @@
 import ServiceDetailPage from "@/components/pages/ServiceDetailPage"
 
-const data = {
-  title: "Service 1",
-  short_desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda illum aliquam quos amet adipisci qui pariatur, hic porro mollitia itaque veritatis ipsum culpa possimus numquam eius. Veniam, mollitia neque!",
-  specification:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda illum aliquam quos amet adipisci qui pariatur, hic porro mollitia itaque veritatis ipsum culpa possimus numquam eius. Veniam, mollitia neque!",
-  image: [
-    {
-      id: 1,
-      title: "Lorem ipsum dolor sit amet.",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 2,
-      title: "Lorem ipsum dolor sit amet.",
-      image: "https://picsum.photos/200/300",
-    },
-  ],
+async function getServiceDetail(id: string) {
+  const res = await fetch(`http://localhost:8002/v1/service/${id}`)
+  if (!res.ok) {
+    throw new Error("Failed to fetch Service data.")
+  }
+  return res.json()
 }
 
-export default function ServiceDetail() {
+export default async function ServiceDetail({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const fetch = await getServiceDetail(params.id)
   return (
     <>
       <ServiceDetailPage
-        title={data.title}
-        short_desc={data.short_desc}
-        description={data.short_desc}
-        image={data.image}
-        specification={data.specification}
+        title={fetch.data.title}
+        short_desc={fetch.data.short_desc}
+        description={fetch.data.short_desc}
+        image={fetch.data.image}
+        specification={fetch.data.specification}
       />
     </>
   )
