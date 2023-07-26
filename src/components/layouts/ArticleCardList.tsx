a3511af6dd80be2d5034f0f4deb3ff3c9b55cf3e
Fandika <fandika.ikhsan@gmail.com>
2023-07-26 18:43:20 +0700

    feat: hover and rounded in article list

--- src/components/layouts/ArticleCardList.tsx
+++ src/components/layouts/ArticleCardList.tsx
@@ -12,7 +12,7 @@ const ArticleCardList: FC<ArticleCardListType> = ({
 }) => {
   return (
     <>
-      <div className="flex gap-4 w-full md:px-4 h-36 md:h-52 text-white p-2">
+      <div className="flex gap-4 w-full md:px-4 h-36 md:h-52 text-white p-2 rounded-lg hover:bg-red-800">
         <div className="relative w- h-full aspect-1">
           <Image
             src={image}
@@ -25,23 +25,22 @@ const ArticleCardList: FC<ArticleCardListType> = ({
             }}
           />
         </div>
-
-        <div className="flex flex-col gap-2 py-2 md:py-5">
-          <div className="">
-            <div className="text-sm md:text-md overflow-hidden font-bold">
-              <p className=" line-clamp-2">{title}</p>
+        <Link href={`/news/${path}`}>
+          <div className="flex flex-col gap-2 py-2 md:py-5 ">
+            <div className="">
+              <div className="text-sm md:text-md overflow-hidden font-bold">
+                <p className=" line-clamp-2">{title}</p>
+              </div>
+              <div className="text-xs text-gray-500">{date}</div>
             </div>
-            <div className="text-xs text-gray-500">{date}</div>
-          </div>
-          <div className="text-xs md:text-sm">
-            <p className="line-clamp-2 md:line-clamp-3">{short_desc}</p>
-          </div>
-          <Link href={`/news/${path}`}>
+            <div className="text-xs md:text-sm">
+              <p className="line-clamp-2 md:line-clamp-3">{short_desc}</p>
+            </div>
             <div className="text-orange-600 underline text-xs md:text-sm">
               Read more
             </div>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
     </>
   )
