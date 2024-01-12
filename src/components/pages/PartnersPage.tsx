4ddf700d9627bbb71853656a01f68e8f372860cd
Fandika <fandika.ikhsan@gmail.com>
2024-01-12 23:25:28 +0700

    feat: revamp the partner layout to only img and text

--- src/components/pages/PartnersPage.tsx
+++ src/components/pages/PartnersPage.tsx
@@ -18,24 +18,46 @@ export default function PartnersPage({
       <PageHeader
         title={title}
         backgroundImage={
-          "https://upload.ocwsb.com/image/banners/partner-header-1.jpg"
+          // "https://upload.ocwsb.com/image/banners/partner-header-1.jpg"
+          "https://picsum.photos/200/300"
         }
         type="tall"
       />
-      <div className="flex flex-col justify-center items-center gap-10 py-10 bg-black">
-        <div className="flex flex-col gap-4 justify-center items-center py-5">
+      <div className="flex flex-col justify-center items-center gap-10 py-10 lg:py-[8rem] bg-black">
+        {/* <div className="flex flex-col gap-4 justify-center items-center py-5">
           <div className="text-sm text-white text-center w-[90%] md:w-[60%]">
             <AnimatedComponent>{description}</AnimatedComponent>
           </div>
-        </div>
+        </div> */}
         <div>
           <AnimatedComponent>
-            <ListGalery
+            {/* <ListGalery
               values={partners}
               page="partners"
               textposition="center"
               textdesktopsize="lg"
-            />
+            /> */}
+            <div>
+              <div className="flex flex-col lg:flex-row justify-center gap-5 items-center">
+                {partners.map((partner, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col justify-center gap-5 items-center"
+                  >
+                    <div className=" overflow-hidden aspect-1 rounded-xl">
+                      <img
+                        src={partner.image}
+                        alt={partner.name}
+                        className="object-cover w-full h-full"
+                      />
+                    </div>
+                    <div>
+                      <h1 className="text-white">{partner.name}</h1>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </AnimatedComponent>
         </div>
       </div>
