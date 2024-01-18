0f3a94343c915a3a00ef8667f4f3c32790bb5398
Fandika <fandika.ikhsan@gmail.com>
2024-01-19 00:44:53 +0700

    feat: refactor listgalerydetail

--- src/components/layouts/ListGaleryDetail.tsx
+++ src/components/layouts/ListGaleryDetail.tsx
@@ -3,132 +3,44 @@ import Image from "next/image"
 import Link from "next/link"
 import { ListGaleryComponentType } from "@/types/CommonType"
 
-const ListGaleryDetail: FC<ListGaleryComponentType> = ({
-  values,
-  page,
-  clickable = false,
-}) => {
+const ListGaleryDetail: FC<ListGaleryComponentType> = ({ values, page }) => {
   return (
     <>
-      <div className="grid grid-cols-2 w-fit m-auto ">
-        {values.map((value, index) =>
-          index % 2 === 0 ? (
-            <React.Fragment key={value.name}>
-              <div
-                // here
-                className={`${
-                  clickable ? "cursor-pointer" : ""
-                } relative h-44 w-44 md:h-64 md:w-96 lg:h-[22rem] lg:w-[30rem] mb-8 shadow-[rgba(0,_0,_0,_0.1)_0px_0px_30px_1px] shadow-[#868686]`}
-              >
-                {clickable ? (
-                  <Link href={`/${page}/${value.id}`}>
-                    <div className="absolute opacity-20 z-10 bg-black hover:bg-red-700 w-full h-full"></div>
-                    <Image
-                      src={value.image}
-                      alt={value.name}
-                      fill={true}
-                      style={{ objectFit: "cover" }}
-                    />
-                  </Link>
-                ) : (
-                  <>
-                    <div className="absolute cursor-default opacity-20 z-10 bg-black w-full h-full"></div>
-                    <Image
-                      src={value.image}
-                      alt={value.name}
-                      fill={true}
-                      style={{ objectFit: "cover" }}
-                    />
-                  </>
-                )}
-              </div>
-              {clickable ? (
-                <Link href={`/${page}/${value.id}`}>
-                  <div className="flex flex-col gap-2">
-                    <div className="px-8 py-8 md:text-sm flex flex-col gap-5 h-44 w-44 md:h-64 md:w-96 lg:h-[22rem] lg:w-[30rem] mb-8 shadow-[rgba(0,_0,_0,_0.1)_0px_0px_30px_1px] shadow-[#868686] bg-[#0A0A0A] text-white text-xs items-start justify-center">
-                      <div className="font-bold lg:text-lg xl:text-2xl">
-                        {value.name}
-                      </div>
-                      <div className="font-base lg:text-base xl:text-lg">
-                        {value.short_desc}
-                      </div>
-                      <div className="underline text-red-800 lg:text-base xl:text-lg">
-                        Learn More
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              ) : (
-                <div className="px-8 py-8 md:text-sm flex flex-col gap-5 h-44 w-44 md:h-64 md:w-96 lg:h-[22rem] lg:w-[30rem] mb-8 shadow-[rgba(0,_0,_0,_0.1)_0px_0px_30px_1px] shadow-[#868686] bg-[#0A0A0A] text-white text-xs items-start justify-center">
-                  <div className="font-bold lg:text-lg xl:text-2xl">
+      <div className="flex flex-col gap-10 px-5">
+        {values.map((value, index) => (
+          <Link href={`/${page}/${value.id}`}>
+            <div
+              className={
+                index % 2 === 0
+                  ? `flex flex-row-reverse shadow-[rgba(5,_0,_0,_5)_10px_10px_15px_0.5px] shadow-[#9c9c9c] h-[14rem] md:h-[18rem]`
+                  : `flex flex-row shadow-[rgba(0,_0,_0,_0.1)_10px_10px_15px_0.5px] shadow-[#9c9c9c] h-[14rem] md:h-[18rem]`
+              }
+            >
+              <div className="flex flex-col items-start justify-center gap-2 bg-[#0A0A0A]  flex-1">
+                <div className="p-10 flex flex-col gap-3">
+                  <div className="text-white font-bold lg:text-lg 2xl:text-xl">
                     {value.name}
                   </div>
-                  <div className="font-base lg:text-base xl:text-lg">
+                  <div className="text-white font-base text-sm lg:text-base">
                     {value.short_desc}
                   </div>
-                  <div className="underline text-red-800 lg:text-base xl:text-lg">
+                  <div className="underline text-red-800 lg:text-base xl:text-base">
                     Learn More
                   </div>
                 </div>
-              )}
-            </React.Fragment>
-          ) : (
-            <React.Fragment key={value.name}>
-              {clickable ? (
-                <Link href={`/${page}/${value.id}`}>
-                  <div className="px-8 py-8 md:text-sm flex flex-col gap-5 h-44 w-44 md:h-64 md:w-96 lg:h-[22rem] lg:w-[30rem] mb-8 shadow-[rgba(0,_0,_0,_0.1)_0px_0px_30px_1px] shadow-[#868686] bg-[#0A0A0A] text-white text-xs items-start justify-center">
-                    <div className="font-bold lg:text-lg xl:text-2xl">
-                      {value.name}
-                    </div>
-                    <div className="font-base lg:text-base xl:text-lg">
-                      {value.short_desc}
-                    </div>
-                    <div className="underline text-red-800 lg:text-base xl:text-lg">
-                      Learn More
-                    </div>
-                  </div>
-                </Link>
-              ) : (
-                <div className="px-8 py-8 md:text-sm flex flex-col gap-5 h-44 w-44 md:h-64 md:w-96 lg:h-[22rem] lg:w-[30rem] mb-8 shadow-[rgba(0,_0,_0,_0.1)_0px_0px_30px_1px] shadow-[#868686] bg-[#0A0A0A] text-white text-xs items-start justify-center">
-                  <div className="font-bold lg:text-lg xl:text-2xl">
-                    {value.name}
-                  </div>
-                  <div className="font-base lg:text-base xl:text-lg">
-                    {value.short_desc}
-                  </div>
-                  <div className="underline text-red-800 lg:text-base xl:text-lg">
-                    Learn More
-                  </div>
-                </div>
-              )}
-              <div
-                className={`relative h-44 w-44 md:h-64 md:w-96 lg:h-[22rem] lg:w-[30rem] shadow-[rgba(0,_0,_0,_0.1)_0px_0px_30px_1px] shadow-[#868686] bg-[#0A0A0A] mb-8`}
-              >
-                {clickable ? (
-                  <Link href={`/${page}/${value.id}`}>
-                    <div className="absolute opacity-20 z-10 bg-black hover:bg-red-700 w-full h-full"></div>
-                    <Image
-                      src={value.image}
-                      alt={value.name}
-                      fill={true}
-                      style={{ objectFit: "cover" }}
-                    />
-                  </Link>
-                ) : (
-                  <>
-                    <div className="absolute cursor-default opacity-20 z-10 bg-black w-full h-full"></div>
-                    <Image
-                      src={value.image}
-                      alt={value.name}
-                      fill={true}
-                      style={{ objectFit: "cover" }}
-                    />
-                  </>
-                )}
               </div>
-            </React.Fragment>
-          )
-        )}
+              <div className="bg-red-300 overflow-hidden flex-1">
+                <Image
+                  src={value.image}
+                  alt={value.name}
+                  width={300}
+                  height={300}
+                  style={{ objectFit: "cover", width: "100%", height: "100%" }}
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </>
   )
