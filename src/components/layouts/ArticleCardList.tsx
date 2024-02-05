7b8d2f8da7968ae37df493f3ef6a00c2a3cc5c32
Fandika <fandika.ikhsan@gmail.com>
2024-02-06 00:02:41 +0700

    feat: add clickable on img news card

--- src/components/layouts/ArticleCardList.tsx
+++ src/components/layouts/ArticleCardList.tsx
@@ -14,16 +14,18 @@ const ArticleCardList: FC<ArticleCardListType> = ({
     <>
       <div className="flex gap-4 w-full md:px-4 h-36 md:h-52 lg:h-64 text-white p-2 rounded-lg hover:bg-red-800 hover:shadow-[rgba(0,_0,_0,_0.1)_0px_0px_15px_3px] hover:shadow-[#868686]">
         <div className="relative h-full aspect-1 md:aspect-2">
-          <Image
-            src={image}
-            alt="Product 1"
-            fill={true}
-            style={{
-              objectFit: "cover",
-              borderRadius: "1rem",
-              aspectRatio: "1/3",
-            }}
-          />
+          <Link href={`/news/${path}`}>
+            <Image
+              src={image}
+              alt="Product 1"
+              fill={true}
+              style={{
+                objectFit: "cover",
+                borderRadius: "1rem",
+                aspectRatio: "1/3",
+              }}
+            />
+          </Link>
         </div>
         <Link href={`/news/${path}`}>
           <div className="flex flex-col gap-2 py-2 md:py-5 ">
